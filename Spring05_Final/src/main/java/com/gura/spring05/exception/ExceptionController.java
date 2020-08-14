9ec1af47aee32e7f22e02de85268e4d757a86025
liyeon <fldus612@gmail.com>
2020-08-14 10:14:45 +0900

    예외처리 필기

--- Spring05_Final/src/main/java/com/gura/spring05/exception/ExceptionController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/exception/ExceptionController.java
@@ -6,24 +6,25 @@ import org.springframework.web.servlet.ModelAndView;
 
 //얘도 bean이 됨 ControllerAdvice 때문에
 //어디든 동작을 할 때 NotDeleteException 타입의 exception이 발생을 하면 실행순서가 여기로 온다.
+
+//예외 컨트롤러를 bean으로 만들기 위한 어노테이션
 @ControllerAdvice
 public class ExceptionController {
 	
+	//Spring Framework가 동작하는 중에 NotDeleteException type의 예외가 발생하면 호출되는 메소드
 	@ExceptionHandler(NotDeleteException.class)
 	public ModelAndView notDelete(NotDeleteException nde) {//exception객체의 참조값이 여기로 들어옴
-		ModelAndView mView = new ModelAndView();
-		mView.addObject("exception", nde);//exception이라는 key값으로 exception의 객체를 넣어줌 
+		//해당 오류가 발생 했을 때 원하는 작업을 한 후
 		
+		//view페이지로 forward이동해서 예외 정보를 응답한다.
+		ModelAndView mView = new ModelAndView();
+		
+		//exception이라는 키값으로 예외 객체를 담고
+		mView.addObject("exception", nde);//exception이라는 key값으로 exception의 객체를 넣어줌 
+		// WEB-INF/view/error/info.jsp 페이지로 forward 이동
 		mView.setViewName("error/info"); //views/error/info.jsp
 		return mView;
 	}
 	
-	@ExceptionHandler(NullPointerException.class)
-	public ModelAndView nullPointer(NullPointerException npe) {//exception객체의 참조값이 여기로 들어옴
-		ModelAndView mView = new ModelAndView();
-		mView.addObject("exception", npe);//exception이라는 key값으로 exception의 객체를 넣어줌 
-		
-		mView.setViewName("error/info"); //views/error/info.jsp
-		return mView;
-	}
+	
 }
